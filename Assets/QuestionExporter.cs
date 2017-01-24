21bfee8af5a55d408854443d3031777f604fe193
Rory Aguilar <raguilar@eatsleepplay.biz>
2017-01-23 20:37:49 -0700

    Initial Commit

--- Assets/QuestionExporter.cs
+++ Assets/QuestionExporter.cs
@@ -7,22 +7,8 @@ public class QuestionExporter : MonoBehaviour {
 
 	[SerializeField] List<TextAsset> m_rawSurveys;
 
-	public static string UnescapeXml( string s )
-	{
-		string unxml = s;
-		if ( !string.IsNullOrEmpty( unxml ) )
-		{
-			// replace entities with literal values
-			unxml = unxml.Replace( "&apos;", "'" );
-			unxml = unxml.Replace( "&quot;", "\"" );
-			unxml = unxml.Replace( "&gt;", "&gt;" );
-			unxml = unxml.Replace( "&lt;", "&lt;" );
-			unxml = unxml.Replace( "&amp;", "&" );
-		}
-		return unxml;
-	}
 
-	Dictionary<string, Dictionary<string,string>> m_surveys;
+	Dictionary<string, Dictionary<string,string>> m_surveys = new Dictionary<string, Dictionary<string,string>>();
 	// Use this for initialization
 	void Start () 
 	{
@@ -33,17 +19,41 @@ public class QuestionExporter : MonoBehaviour {
 
 			string line = null;
 			string workTitle = "";
+			string lastAnswer = "";
+			string lastPoints = "";
+
+			bool nextAnswer = true;
 
 			while( (line = surveyBuff.ReadLine()) != null)
 			{
+				int beginIdx = line.IndexOf('>') + 1;
+				int endIdx = line.IndexOf('<',beginIdx);
+
+				if(workTitle != "" && beginIdx == endIdx)
+					continue;
+
 				// Found a new survey
 				if(line.Contains("<td class=\"column-2\">"))
 				{
-					int beginIdx = line.IndexOf('>') + 1;
-					int endIdx = line.IndexOf('<',beginIdx);
-
 					workTitle = line.Substring(beginIdx, endIdx - beginIdx);
-					workTitle = UnescapeXml(workTitle);
+					m_surveys.Add(workTitle, new Dictionary<string, string>());
+				}
+				else if(workTitle != "" && !line.Contains("</tr>"))
+				{
+					if(nextAnswer)
+					{
+						lastAnswer = line.Substring(beginIdx, endIdx - beginIdx);
+					}
+					else
+					{
+						lastPoints = line.Substring(beginIdx, endIdx - beginIdx);
+						m_surveys[workTitle].Add(lastAnswer,lastPoints);
+					}
+					nextAnswer = !nextAnswer;
+				}
+				else if(line.Contains("</tr>"))
+				{
+						workTitle = "";
 				}
 			}
 		}
